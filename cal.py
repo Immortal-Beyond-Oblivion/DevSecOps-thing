a038fe128eb475afe72d65c38510955e5aab385c
Immortal-Beyond-Oblivion <yourlimitisyourknowledge@gmail.com>
2026-01-15 12:13:54 +0530

    Added Multiply function

--- cal.py
+++ cal.py
@@ -14,3 +14,20 @@ def div(a: int | float, b: int | float) -> float:
         raise ZeroDivisionError("divide by zero.")
 
     return a / b
+
+
+def multiply(a: float, b: float) -> float:
+    """
+    Multiply two numbers and return the result.
+
+    Args:
+        a (float): First number
+        b (float): Second number
+
+    Returns:
+        float: Product of a and b
+
+    Author: Amritanshu Aditya 23BCS013
+    """
+    return a * b
+
